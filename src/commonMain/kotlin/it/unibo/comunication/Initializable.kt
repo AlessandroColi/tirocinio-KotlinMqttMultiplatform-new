ba7c65abba9f2fabd19e51c8367f38026e427656
Coli Alessandro <alessandro.coli2@studio.unibo.it>
2024-04-18 14:01:09 +0200

    build(Initializable): added doc to Initializable

--- src/commonMain/kotlin/it/unibo/comunication/Initializable.kt
+++ src/commonMain/kotlin/it/unibo/comunication/Initializable.kt
@@ -2,8 +2,17 @@ package it.unibo.comunication
 
 import arrow.core.Either
 
+/**
+ * Represents something that needs to be initialized and finalized.
+ */
 interface Initializable {
 
+    /**
+     * Performs the operations needed before using the object
+     */
     suspend fun initialize(): Either<ProtocolError, Unit>
+    /**
+     * Performs the closing operations on the object
+     */
     fun finalize(): Either<ProtocolError, Unit>
 }
