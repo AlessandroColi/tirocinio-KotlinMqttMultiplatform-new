4fc31049743d62b9221da13470f59fbc87162544
Coli Alessandro <alessandro.coli2@studio.unibo.it>
2024-04-27 11:31:17 +0200

    fix(mqttJS): works for writing but not for reading

--- src/jsMain/kotlin/it/unibo/comunication/MqttJsInterface.kt
+++ src/jsMain/kotlin/it/unibo/comunication/MqttJsInterface.kt
@@ -1,72 +1,55 @@
+@file:Suppress("REDUNDANT_NULLABLE")
 package it.unibo.comunication
-
 /**
  * js function to import a library, only used for mqtt.js in this project.
  */
-external fun require(module: String): MqttJs
-
+external fun require(module: String): dynamic
 /**
  * represents a mqtt client.
  */
 external interface MqttJsClient {
     /**
-     * setups what to do on cenrtain events.
-     * @param event the trigger for the callback function (message,connect,...).
-     * @param callback the function to be executed after the given event.
-     */
-    fun on(event: String, callback: (String, dynamic, dynamic) -> Unit)
-
-    /**
-     * subscribe to the given topic.
-     * @param topic the topic.
-     * @param options the message options (qous, retain, ...).
-     * @param callback the funtion to be executed upon disconnect and connect.
-     */
-    fun subscribe(topic: String, options: dynamic, callback: (err: dynamic, granted:dynamic) -> Unit)
-
-    /**
      * publish a message on the given topic.
      * @param topic the topic.
      * @param message the message.
-     * @param options the message options (qous, retain, ...).
+     * @param options the message options (qos, retain, ...)
+     * @param callback the function to be called upon complete.
      */
-    fun publishAsync(topic: String, message: dynamic,
-                options: dynamic  = definedExternally )
-
+    fun publish(
+        topic: String,
+        message: dynamic,
+        options: dynamic? = definedExternally,
+        callback: ((err:dynamic) -> Unit)? = definedExternally
+    )
+    /**
+     * subscribe to the given topic.
+     * @param topic the topic.
+     * @param options the message options (qos, retain, ...).
+     * @param callback the function to be executed upon disconnect and connect.
+     */
+    fun subscribe(topic: String, options: dynamic? = definedExternally,
+                  callback: ((err: dynamic, granted: dynamic) -> Unit)? = definedExternally)
+    /**
+     * setups what to do on certain events.
+     * @param event the trigger for the callback function (message,connect,...).
+     * @param callback the function to be executed after the given event.
+     */
+    fun on(event: String, callback: (args: Array<dynamic>) -> Unit)
     /**
      * closes the client.
+     * @param force the end needs to be forced or not
+     * @param callback the function to be called upon complete.
      */
-    fun end()
+    fun end(force: Boolean? = definedExternally, callback: (() -> Unit)? = definedExternally)
 }
 
 /**
- * connection options for the mqtt broker.
- */
-external interface MqttJsOptions {
-    /**
-     * the username of the client.
-     */
-    var username: String
-
-    /**
-     * the password.
-     */
-    var password: String
-
-    /**
-     * cleanstart of the connection.
-     */
-    var clean : Boolean
-}
-
-/**
- * represents the connection to the mqtt broker in js.
+ * the connection to the broker.
  */
 external interface MqttJs {
     /**
      * connect to the broker.
      * @return the client [MqttJsClient].
      */
-    fun connect(url: String, port: Int,  options: dynamic = definedExternally): MqttJsClient
+    fun connect(host: String, options: dynamic? = definedExternally): MqttJsClient
 }
-
--- src/jsMain/kotlin/it/unibo/comunication/MqttProtocol.kt
+++ src/jsMain/kotlin/it/unibo/comunication/MqttProtocol.kt
@@ -1,124 +1,125 @@
-package it.unibo.comunication
+    package it.unibo.comunication
 
-import arrow.core.Either
-import arrow.core.raise.either
-import arrow.core.raise.ensureNotNull
-import arrow.core.right
-import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+    import arrow.core.Either
+    import arrow.core.raise.either
+    import arrow.core.raise.ensureNotNull
+    import arrow.core.right
+    import io.github.oshai.kotlinlogging.KotlinLogging
+    import kotlinx.coroutines.CoroutineDispatcher
+    import kotlinx.coroutines.flow.Flow
+    import kotlinx.coroutines.flow.MutableSharedFlow
+    import kotlinx.coroutines.flow.asSharedFlow
 
-/**
- * Represents the MQTT protocol.
- */
-@Suppress("unused")
+    /**
+     * Represents the MQTT protocol.
+     */
+    @Suppress("unused")
 
-actual class MqttProtocol actual constructor(
-    private val host: String,
-    private val port: Int,
-    private val username: String?,
-    private val password: String?,
-    private val mainTopic: String,
-    private val coroutineDispatcher: CoroutineDispatcher
-) : Protocol {
+    actual class MqttProtocol actual constructor(
+        private val host: String,
+        private val port: Int,
+        private val username: String?,
+        private val password: String?,
+        private val mainTopic: String,
+        private val coroutineDispatcher: CoroutineDispatcher
+    ) : Protocol {
 
-    private val logger = KotlinLogging.logger("MqttProtocol")
+        private val logger = KotlinLogging.logger("MqttProtocol")
 
-    private val registeredTopics = mutableMapOf<Pair<Entity, Entity>, String>()
-    private val topicChannels = mutableMapOf<String, MutableSharedFlow<ByteArray>>()
-    private lateinit var client : MqttJsClient
-    private lateinit var mqtt : MqttJs
+        private val registeredTopics = mutableMapOf<Pair<Entity, Entity>, String>()
+        private val topicChannels = mutableMapOf<String, MutableSharedFlow<ByteArray>>()
+        private lateinit var client : MqttJsClient
+        private lateinit var mqtt : MqttJs
 
-    actual override suspend fun setupChannel(
-        source: Entity,
-        destination: Entity
-    ) {
-        println( "Setting up channel for entity $source" )
-        registeredTopics += (source to destination) to toTopics(source, destination)
-        registeredTopics += (destination to source) to toTopics(destination, source)
-        topicChannels += toTopics(source, destination) to MutableSharedFlow(1)
-        topicChannels += toTopics(destination, source) to MutableSharedFlow(1)
+        actual override suspend fun setupChannel(
+            source: Entity,
+            destination: Entity
+        ) {
+            println( "Setting up channel for entity $source to $destination" )
+            registeredTopics += (source to destination) to toTopics(source, destination)
+            registeredTopics += (destination to source) to toTopics(destination, source)
+            topicChannels += toTopics(source, destination) to MutableSharedFlow(1)
+            topicChannels += toTopics(destination, source) to MutableSharedFlow(1)
 
-    }
+        }
 
-    actual override suspend fun writeToChannel(
-        from: Entity,
-        to: Entity,
-        message: ByteArray
-    ): Either<ProtocolError, Unit>  = either {
-        val topic = registeredTopics[Pair(from, to)]
-        println( "Writing message $message to topic $topic" )
+        actual override suspend fun writeToChannel(
+            from: Entity,
+            to: Entity,
+            message: ByteArray
+        ): Either<ProtocolError, Unit>  = either {
+            val topic = registeredTopics[Pair(from, to)]
+            println( "Writing message ${message.decodeToString()} to topic $topic" )
 
-        ensureNotNull(topic) { ProtocolError.EntityNotRegistered(to) }
+            ensureNotNull(topic) { ProtocolError.EntityNotRegistered(to) }
 
-        Either.catch { client.publishAsync(
-            topic,
-            message,
-            options = object {
-                var qos = 2
-                var retain = true
-            }
-        )
-        }.mapLeft { ProtocolError.ProtocolException(it) }
-    }
+            Either.catch { client.publish(
+                topic,
+                message.decodeToString(),
+                options = object {
+                    var qos = 2
+                    var retain = true
+                }
+            )   }.mapLeft { ProtocolError.ProtocolException(it) }
+        }
 
-    actual override fun readFromChannel(
-        from: Entity,
-        to: Entity
-    ): Either<ProtocolError, Flow<ByteArray>> = either {
-        val candidateTopic = ensureNotNull(registeredTopics[Pair(from, to)]) { ProtocolError.EntityNotRegistered(from) }
-        val channel = ensureNotNull(topicChannels[candidateTopic]) { ProtocolError.EntityNotRegistered(from) }
-        println( "Reading from topic $candidateTopic" )
-        channel.asSharedFlow()
-    }
-    actual override suspend fun initialize(): Either<ProtocolError, Unit> = either {
-        Either.catch {
-            mqtt = require("mqtt")
+        actual override fun readFromChannel(
+            from: Entity,
+            to: Entity
+        ): Either<ProtocolError, Flow<ByteArray>> = either {
+            val candidateTopic = ensureNotNull(registeredTopics[Pair(from, to)])
+                { ProtocolError.EntityNotRegistered(from) }
+            val channel = ensureNotNull(topicChannels[candidateTopic])
+                { ProtocolError.EntityNotRegistered(from) }
+            println( "Reading from topic $candidateTopic" )
+            channel.asSharedFlow()
+        }
+        actual override suspend fun initialize(): Either<ProtocolError, Unit> = either {
+            Either.catch {
+                println("entering init")
+                @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
+                mqtt = require("mqtt") as MqttJs
 
-            client = mqtt.connect(host, port, username?.takeIf { it.isNotEmpty() }?.let { safeUsername ->
-                password?.takeIf { it.isNotEmpty() }?.let { safePassword ->
-                    object : MqttJsOptions {
-                        override var username: String = safeUsername
-                        override var password: String = safePassword
-                        override var clean: Boolean = false
+                client = mqtt.connect("mqtt://${host}:${port}")
+                client.on("connect") { _ ->
+                    println("client initialized")
+                    client.subscribe("${mainTopic}/#",
+                        options = object {
+                            val qos = 2
+                        }) { err: dynamic, granted: dynamic ->
+                        if (!err as Boolean) {
+                            println( "Subscribed to topic: ${granted.topic}" )
+                        } else {
+                            println( "connect did not work: ${err.message}" )
+                        }
                     }
                 }
-            })
-            client.on("connect") { _,_,_ ->
-                client.subscribe("${mainTopic}/#",
-                    options = object {
-                        val qos = 2
-                    }) { err: dynamic, granted: dynamic ->
-                    if (err != null) {
-                        println( "disconnected: ${err.message}" )
-                    } else {
-                        println( "Subscribed to topic: ${granted.topic}" )
-                    }
+
+                client.on("error") { error: dynamic ->
+                    println("Error connecting to MQTT broker: ${error.message}")
+                }
+                //TODO forse vanno messi gli on() anche per le altre casistiche?
+                client.on("message"){
+                        (topic: String, message: dynamic, _) ->
+                    val msg = message.toString()
+                    println("Received message on topic $topic: $msg")
+                    topicChannels[topic as String]?.tryEmit(msg.encodeToByteArray())
                 }
             }
-            //TODO forse vanno messi gli on() anche per le altre casistiche?
-            client.on("message"){
-                topic: String, message: dynamic, packet: dynamic ->
-                logger.debug{"Received message on topic $topic: $message"}
-                topicChannels[topic]?.tryEmit(packet.payload.toByteArray()) //va bene ?
+        }
+
+        actual override fun finalize(): Either<ProtocolError, Unit> {
+            client.end()
+            println( "client finalized" )
+            return Unit.right()
+        }
+
+        //todo comune a tutte le piattaforme, la posso mettere in common?
+        private fun toTopics(source: Entity, destination: Entity): String {
+            return if (source.id != null && destination.id != null) {
+                "${mainTopic}/${source.entityName}/${destination.entityName}/${destination.id}"
+            } else {
+                "${mainTopic}/${source.entityName}/${destination.entityName}"
             }
         }
     }
-
-    actual override fun finalize(): Either<ProtocolError, Unit> {
-        client.end()
-        println( "client finalized" )
-        return Unit.right()
-    }
-
-    //todo comune a tutte le piattaforme, la posso mettere in common?
-    private fun toTopics(source: Entity, destination: Entity): String {
-        return if (source.id != null && destination.id != null) {
-            "${mainTopic}/${source.entityName}/${destination.entityName}/${destination.id}"
-        } else {
-            "${mainTopic}/${source.entityName}/${destination.entityName}"
-        }
-    }
-}
